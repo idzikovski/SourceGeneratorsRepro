353cc813883d848a15769683be23d1c9805c5ad4
Ivan Djikovski <ivan.djikovski@wsa.com>
2022-09-14 11:27:28 +0200

    added repro project

---
+++ TestApp/ViewModels/AboutViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace TestApp.ViewModels
+{
+    [INotifyPropertyChanged]
+    public class AboutViewModel
+    {
+        private string _title;
+
+        public AboutViewModel()
+        {
+            Title = "About";
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+        }
+
+        public ICommand OpenWebCommand { get; }
+    }
+}
